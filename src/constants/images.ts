1d69e5c1af91f418c06cfe2786a10d9cbec940cf
pnewsam <paul.newsam@gmail.com>
2024-12-14 08:30:23 -0800

    Add a few photos, imagegrid

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,4 +1,4 @@
-type Image = {
+export type Image = {
   src: string;
   alt: string;
   width: number;
@@ -41,11 +41,67 @@ export const images: Image[] = [
     location: "Galapagos",
   },
   {
+    src: "https://storage.paultnewsam.com/albatross_1_optimized.webp",
+    alt: "Albatross",
+    width: 1600,
+    height: 1067,
+    caption: "Albatross",
+    location: "Galapagos",
+  },
+  {
+    src: "https://storage.paultnewsam.com/birds_1_optimized.webp",
+    alt: "Birds",
+    width: 1067,
+    height: 1600,
+    caption: "Birds",
+    location: "Galapagos",
+  },
+  {
+    src: "https://storage.paultnewsam.com/booby_1_optimized.webp",
+    alt: "Nazca Booby",
+    width: 1067,
+    height: 1600,
+    caption: "Nazca Booby",
+    location: "Galapagos",
+  },
+  {
     src: "https://storage.paultnewsam.com/heron_2_optimized.webp",
     alt: "Heron",
     width: 1600,
     height: 1067,
     caption: "Heron",
+    location: "Galapagos",
+  },
+  {
+    src: "https://storage.paultnewsam.com/boobies_2_optimized.webp",
+    alt: "Nazca Boobies",
+    width: 1067,
+    height: 1600,
+    caption: "Nazca Boobies",
+    location: "Galapagos",
+  },
+  {
+    src: "https://storage.paultnewsam.com/gull_1_optimized.webp",
+    alt: "Gull",
+    width: 1067,
+    height: 1600,
+    caption: "Gull",
+    location: "Galapagos",
+  },
+  {
+    src: "https://storage.paultnewsam.com/booby_2_optimized.webp",
+    alt: "Nazca Booby",
+    width: 1600,
+    height: 1067,
+    caption: "Nazca Booby",
+    location: "Galapagos",
+  },
+  {
+    src: "https://storage.paultnewsam.com/coastline_1_optimized.webp",
+    alt: "Coastline",
+    width: 1600,
+    height: 1067,
+    caption: "Coastline",
     location: "Galapagos",
   },
 ];
